1937804d34a27b85d38f6e610fdd239229b88d72
iqrar haider <iqrar@iqrars-MacBook-Pro.local>
2018-04-21 00:22:31 +0200

    Toolbar Hide and show

--- HidingToolbar/ViewController.swift
+++ HidingToolbar/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIScrollViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var toolbar: UIToolbar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.webView.scrollView.delegate = self
+        let url = URL(string: "https://github.com/iqrar")
+        if let unwrappedURL = url {
+            
+            let request = URLRequest(url: unwrappedURL)
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request) { (data, response, error) in
+                
+                if error == nil {
+                    DispatchQueue.main.async {
+                        self.webView.loadRequest(request)
+                    }
+                    
+                } else {
+                    
+                    print("ERROR: \(error)")
+                    
+                }
+                
+            }
+            
+            task.resume()
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.panGestureRecognizer.translation(in: scrollView).y < 0 {
+            
+            self.toolbar.isHidden = true
+        } else {
+            
+            self.toolbar.isHidden = false
+            
+        }
+        
+    }
+    
 }
 
